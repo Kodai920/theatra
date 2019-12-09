60b0d30c1a41e422cca2c02a7a7040a49f3099e0
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-09 12:19:17 +0530

    20191209

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hey', function(){
-    return view('index');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
