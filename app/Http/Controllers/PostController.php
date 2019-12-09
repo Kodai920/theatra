e91c57f4457864495e0584344bf8e44d3dbba4b0
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-09 12:49:19 +0530

    20191209

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,8 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-        return view('posts.index')->with('posts',$posts);
+        return view('posts.index');
     }
 
     /**
