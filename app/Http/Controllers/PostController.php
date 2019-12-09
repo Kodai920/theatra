76f0a7c2ebce836f4da32d44f2e82f29fc087102
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-09 22:14:15 +0530

    20191209

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -82,7 +82,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        return view('posts.create')->with('post',$post);
+        return view('posts.create');
     }
 
     /**
