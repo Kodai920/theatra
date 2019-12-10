13db01f9b5c102b5ea0eee8c2463652c8dd3f6a5
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-10 23:11:52 +0530

    20191209

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,38 +2,43 @@
 @section('content')
 
 <div class="card">
-    <div class="card-header">{{isset($posts) ? 'Create Post' : 'Edit Post'}}</div>
+<div class="card-header">Create Post</div>
 
-    <div class="card-body">
-        @if(count($errors) > 0)
+<div class="card-body">
+    @if(count($errors) > 0)
         <ul class="list-group">
             @foreach($errors->all() as $error)
-            <li class="list-group-item text-danger"> {{$error}} </li>
+                <li class="list-group-item text-danger">{{$error}}</li>
             @endforeach
         </ul>
-        @endif
+    @endif
 
-        <form action="{{isset($post) ? route('posts.store') : route('posts.update',[$post->id])}}" method="post" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" name="title" class="form-control" value="{{isset($post) ? '' : $post->title}} ">
-            </div>
+    <form action="{{route('posts.store')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control">
+        </div>
 
-            <div class="form-group">
-                <label for="about">Impression</label>
-                <textarea name="about" cols="30" rows="5" class="form-control">{{isset($post) ? '' : $post->about}}</textarea>
-            </div>
+        <div class="form-group">
+            <label for="about" >Impression</label>
+            <br>
+            <textarea name="about" class="form-control" rows="5"></textarea>
+        </div>
 
-            <div class="form-group">
-                <label for="about">Featured image</label>
-                <input type="file" name="featured" class="form-control-file">
-            </div>
+        <div class="form-group">
+            <label for="about">image</label>
+            <input type="file" name="featured" class="form-control-file">
+        </div>
 
-            <div class="form-group">
-                <button class="btn btn-success btn-block" type="submit">{{isset($posts) ?'Store Post' : 'Update Post'}} </button>
-            </div>
-        </form>
+        <div class="form-group">
+            <button type="submit" class="btn btn-success btn-block">POST</button>
+        </div>
+    </form>
+</div>
+</div>
+
+@endsection
     </div>
 </div>
 
