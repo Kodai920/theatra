bc6424c5caab139e566798ae4b721d7ddf707561
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-12 14:49:16 +0530

    20191209

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UpdateProfileRequest;
 
 class ProfileController extends Controller
 {
@@ -12,7 +13,30 @@ class ProfileController extends Controller
         return view('profiles.index')->with('user',Auth::user());
     }
 
-    public function update(){
+    public function update(UpdateProfileRequest $request){
+        $user = Auth::user();
 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->about = $request->about;
+
+        $user->save();
+        $user->profile->save();
+
+        if($request->hasFile('avator')){
+            $avator = $request->avator;
+            $avator_new_name = time().$avator->getClientOriginalName();
+            $avator->move('uploads/avator/',$avator_new_name);
+            $user->profile->avator = 'uploads/avator/'.$avator_new_name;
+            $user->profile->save();
+        }
+
+        if($request->has('password')){
+            $user->password = bcrypt($request->password);
+            $user->save();
+        }
+
+        Session::flash('success','更新完了');
+        return redirect()->back();
     }
 }
