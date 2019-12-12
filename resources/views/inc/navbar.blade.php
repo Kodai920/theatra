d276258ca8b5e9530b5dd8df7bc993522931a474
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-12 11:04:37 +0530

    20191209

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -36,7 +36,11 @@
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
-                                {{__('Profile')}}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Profile') }}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
