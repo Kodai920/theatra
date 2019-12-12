21c7901635da5d0ab6740f9c373adef6521b0cec
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-12 11:47:21 +0530

    20191209

--- app/Profile.php
+++ app/Profile.php
@@ -9,4 +9,8 @@ class Profile extends Model
     protected $table = 'profiles';
 
     protected $fillable = ['avator','about','user_id'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
