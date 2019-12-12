88ff3020df978d7b1c4091a43f42e66a41d680d5
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-12 11:48:32 +0530

    20191209

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
