4e4119f0b9e91c8c3caaa97a1fc3e96915fcdf72
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-14 00:48:17 +0530

    20191209

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -12,11 +12,9 @@
         </ul>
         @endif
 
-        <form action="{{route('profiles.update'
-        // ,[$user->id]
-        )}}" method="post" enctype="multipart/form-data">
+        <form action="{{route('profiles.update',[$user->id])}}" method="post" enctype="multipart/form-data">
             @csrf
-            {{-- @method('PUT') --}}
+            @method('PUT')
             <div class="form-group">
                 <label for="name">アカウント名</label>
                 <input type="text" name="name" value="{{Auth::user()->name}}" class="form-control">
