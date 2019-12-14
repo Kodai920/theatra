c65fdc3f70cb92de4c47aafffa3622f442fd4805
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-14 12:36:05 +0530

    20191209

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -36,7 +36,7 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('profiles.index') }}">
-                                Profile
+                                プロフィール
                             </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
