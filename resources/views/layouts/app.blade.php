9b1a91551399acbf2c4673fca50b2d776736f238
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-14 11:15:36 +0530

    20191209

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Theatra') }}</title>
+    <title>{{ config('', 'Theatra') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
