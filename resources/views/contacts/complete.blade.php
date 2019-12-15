3590dc99315b096b12c42e1a346e3f832ab05c99
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-15 15:33:55 +0530

    20191209

--- resources/views/contacts/complete.blade.php
+++ resources/views/contacts/complete.blade.php
@@ -5,7 +5,8 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">お問い合わせ</div>
+                <div class="panel-heading">送信完了</div>
+                <div></div>
                 <div class="panel-body">
                     お問い合わせいただき、大変ありがとうございます。<br>
                     お客様のご要望を取り入れ、サービスの質の向上に努めてまいります。
