4a937840a07fec69131243bc7676eed5940caede
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-15 18:40:48 +0530

    20191209

--- config/app.php
+++ config/app.php
@@ -139,6 +139,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Bestmomo\LaravelEmailConfirmation\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
