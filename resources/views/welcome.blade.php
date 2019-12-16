45d905a5f3f21960227cfbfa4fddae5d5d437746
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-17 00:53:40 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
     <div class="wrapper">
         <div class="welcome-image">
             <h1 class="welcome-text">さあ、最高のNETFLIXライフを。</h1>
-            <p><a href="{{ route('register') }}" class="btn btn-lg btn-register">今すぐはじめる</a></p>
+            <a href="{{ route('register') }}" class="btn btn-lg btn-register">今すぐはじめる</a>
         </div>
     </div>
     @include('inc.footer')
