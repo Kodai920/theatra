765b790734262b55ce2f6d2ff31a8c3131ff8571
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-16 19:11:57 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="wrapper">
+    <div class="row">
+        <div class="col-8">
+            <img src="profile.jpeg" alt="">
+        </div>
+    </div>
+</div>
 
 @endsection
