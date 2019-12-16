45682639f4dd19aab42a526b5aa19ff0c3e45571
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-16 21:53:53 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,48 @@
-@extends('layouts.app')
-@section('content')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div class="wrapper color-info">
-    <div>
-        <h1 class="welcome-image">さあ、最高のNETFLIXライフを始めましょう</h1>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('', 'Theatra') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+</head>
+<body>
+    <div id="app">
+    @include('inc.navbar')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div>
+                <h1 class="welocome-image">さあ、最高のNETFLIXライフを</h1>
+            </div>
+        </div>
     </div>
-</div>
-
-@endsection
+        @include('inc.footer')
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if(Session::has('success'))
+        toastr.success(" {{Session::get('success')}} ")
+        @endif
+        @if(Session::has('info'))
+        toastr.info(" {{Session::get('info')}} ")
+        @endif
+    </script>
+</body>
+</html>
