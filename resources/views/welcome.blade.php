19c45c666714d4f04ce55a0d2feef559f33ace26
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-16 19:02:28 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Teatra</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">ホーム</a>
-                    @else
-                        <a href="{{ route('login') }}">ログイン</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">新規登録</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Theatra
-                </div>
-
-                <div class="links">
-                    <a href="https://www.netflix.com/" target="_blank">Netflix</a>
-                    <a href="https://www.amazon.co.jp/piv/" target="_blank">Prime</a>
-                    <a href="https://www.hulu.jp/" target="_blank">Hulu</a>
-                    <a href="https://video.unext.jp/" target="_blank">U-NEXT</a>
-                    <a href="https://pc.video.dmkt-sp.jp/" target="_blank">dTV</a>
-                    <a href="https://fod.fujitv.co.jp/s/" target="_blank">FOD</a>
-                    <a href="https://www.paravi.jp" target="_blank">Paravi</a>
-                    <a href="https://www.youtube.com" target="_blank">Youtube</a>
-                </div>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
