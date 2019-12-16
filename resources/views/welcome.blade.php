e76165af9401d205a0befe8d6f4375fa83cb16ee
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-16 19:49:14 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 
 <div class="wrapper">
     <div class="row">
-        <div class="col-8">
-            <img src="theater1.png" alt="">
+        <div class="col-12">
+            <img src="theater1.png" alt="背景画像">
         </div>
     </div>
 </div>
