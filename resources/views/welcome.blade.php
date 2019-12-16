5f00dfea9e05b9d8a90da1b5efa9456a8ff34af9
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-16 21:46:51 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="wrapper">
+<div class="wrapper color-info">
     <div>
         <h1 class="welcome-image">さあ、最高のNETFLIXライフを始めましょう</h1>
     </div>
