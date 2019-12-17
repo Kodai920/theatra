848e981dd124a7de77ed48576d8b19081165db4f
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-17 21:06:14 +0530

    20191209

---
+++ database/migrations/2019_12_17_131529_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('about');
+            $table->integer('year');
+            $table->integer('time');
+            $table->string('integer');
+            $table->string('director');
+            $table->string('writer');
+            $table->string('cast');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
