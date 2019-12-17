c41a05bc5f9c05158ce3b3f4379106a47ad19214
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-17 17:07:26 +0530

    20191209

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -27,7 +27,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        return view('cateogries.create');
+        return view('categories.create');
     }
 
     /**
