6e1fffca83fcb0ac0218bc9032f6fbaba03c2791
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-17 10:08:15 +0530

    20191216

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                 </div>
             </div>
         </div>
-        <hr size="80%" color="#131313">
+        <hr size="50%%" color="#131313">
     </div>
     @include('inc.footer')
     </div>
