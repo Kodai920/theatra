018743bb0e4adbebcb2def50c42b736020810eee
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 22:40:54 +0530

    20191209

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name'];
+
     public function movies(){
         return $this->belongsToMany('App\Movie','movie_category','movie_id','category_id');
     }
--- app/Country.php
+++ app/Country.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
+    protected $fillable = ['name'];
+
     public function movies(){
         return $this->hasMany('App\Movie');
     }
