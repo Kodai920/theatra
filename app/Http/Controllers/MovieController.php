dde9e1304843dd6ee92d4d508fb731bd956b0c82
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 16:28:13 +0530

    20191209

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -47,9 +47,9 @@ class MovieController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Movie $movie)
     {
-        //
+        return view('movies.show')->with('movies',$movies);
     }
 
     /**
---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="card">
+    <div class="card-header">投稿</div>
+
+    <div class="card-body">
+    <table class="table table-hover">
+        <thead>
+            <th></th>
+            <th>タイトル</th>
+        </thead>
+        <tbody>
+            @if($posts->count() > 0)
+            @foreach($posts as $post)
+            <tr>
+                <td><img src="{{$post->featured_img}}" width="80px" height="80px"></td>
+                <td>
+                    {{$post->title}}
+                </td>
+                <td>
+                    <a class="btn btn-primary" href="{{route('posts.show',[$post->id])}}"><i class="fas fa-eye"></i></a>
+                </td>
+                @if(Auth::check())
+                <td>
+                    <a class="btn btn-info" href="{{route('posts.edit',[$post->id])}}"><i class="fas fa-edit"></i></a>
+                </td>
+                <td>
+                    <form action="{{route('posts.destroy',[$post->id])}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                    </form>
+                </td>
+                @endif
+            </tr>
+            @endforeach
+            @else
+            <tr>
+                <th colspan=2 class="text-center"><a href="{{route('posts.create')}}">初めての投稿をしましょう！</a></th>
+            </tr>
+            @endif
+        </tbody>
+    </table>
+</div>
+
+@endsection
