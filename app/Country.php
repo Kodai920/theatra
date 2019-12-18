84bdd7d51bb4e13cff3e561d1041a98d97fa93dc
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 11:49:12 +0530

    20191218

---
+++ app/Country.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    public function movies(){
+        return $this->hasMany('App\Movie');
+    }
+}
--- app/Movie.php
+++ app/Movie.php
@@ -9,4 +9,12 @@ class Movie extends Model
     public function categories(){
         return $this->belongsToMany('App\Category','movie_category','movie_id','category_id');
     }
+
+    public function country(){
+        return $this->belongsTo('App\Country');
+    }
+
+    public function genre(){
+        return $this->belongsTo('App\Genre');
+    }
 }
