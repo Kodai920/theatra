55640893821fd6788e7c9372eb21eff492cff7e0
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 15:04:05 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
-    {{-- <table class="table table-hover">
-        <tbody> --}}
             @foreach($movies as $movie)
-            {{-- <tr>
-            <td> --}}
                 <div>
-                    <a href="#" class="float-right m-4">
+                    <a href="#" class="float-left m-4">
                     <img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
                     </a>
                 </div>
-            {{-- </td>
-            </tr> --}}
             @endforeach
-        {{-- </tbody>
-    </table> --}}
 
 @endsection
