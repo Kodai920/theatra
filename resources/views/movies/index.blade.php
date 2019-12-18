6aba557c8dd8923370b8f51404e6b42ea289e778
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 14:40:10 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,7 +6,7 @@
             @foreach($movies as $movie)
             {{-- <tr>
             <td> --}}
-                <img src="{{'movies/'.$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
+                <img src="{{'movies'.$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
             {{-- </td>
             </tr> --}}
             @endforeach
