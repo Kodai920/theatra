68e3dbd33df51002b75b3e3d705c64ab67331492
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 16:57:50 +0530

    20191209

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,6 +4,14 @@
 <div class="card">
     <div class="card-header">{{$movie->title}}({{$movie->year}}年)</div>
     <div class="card-body">
+        <div class="row">
+            <div class="col-5">
+                <img src="{{$movie->image}}" alt="{{$movie->title}}" width="210px" height="300px">
+            </div>
+            <div class="col-7">
+
+            </div>
+        </div>
     </div>
 </div>
 
