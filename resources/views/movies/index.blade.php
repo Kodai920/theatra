05e58ba301fccd955453d361215078cd2661f163
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 14:21:12 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,19 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="card">
-        <div class="card-header">映画</div>
-
-        <div class="card-body">
-        <table class="table table-hover">
-            <tbody>
-                @foreach($movies as $movie)
-                <tr>
-                <td><img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}"></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <table class="table table-hover">
+        <tbody>
+            @foreach($movies as $movie)
+            <tr>
+            <td><img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}"></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
