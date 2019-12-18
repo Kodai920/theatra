a9d3b6111cde600fdc7800d163bcc06008dc9632
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 15:21:59 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,7 @@
             @foreach($movies as $movie)
                 <div id="movie-image">
                     <a href="#" class="float-left m-4">
-                    <img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
+                    <img src="{{$movie->image}}"alt="{{$movie->title}}">
                     </a>
                 </div>
             @endforeach
