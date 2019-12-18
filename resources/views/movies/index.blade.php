64414b006b9186eb651f0f71a62f6b1f4b0557c1
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-18 16:32:29 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
-            @foreach($movies as $movie)
-                <div id="movie-image">
-                    <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
-                    <img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
-                    </a>
-                </div>
-            @endforeach
+    @foreach($movies as $movie)
+        <div id="movie-image">
+            <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
+            <img src="{{$movie->image}}" width="210px" height="300px" alt="{{$movie->title}}">
+            </a>
+        </div>
+    @endforeach
 
 @endsection
