69975e1b6a7affed084a56c10ab21df86d9b8d87
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 11:12:31 +0530

    20191209

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -10,8 +10,8 @@
             </div>
             <div class="col-7">
                 <ul>
-                    {{-- <li>制作国 : {{$movie->country->name}}</li>
-                    <li>ジャンル : {{$movie->category->name}}</li> --}}
+                    <li>制作国 : {{$movie->country->name}}</li>
+                    <li>ジャンル : {{$movie->category->name}}</li>
                     <li>上映時間 : {{$movie->time}}分</li>
                 </ul>
                 <div>
@@ -26,6 +26,7 @@
             <p>{{$movie->about}}</p>
         </div>
         <div class="col-7">
+            <h3>制作</h3>
             <div> 監督 : {{$movie->director}} </div>
             <div> 脚本 : {{$movie->writer}} </div>
             <div> キャスト : {{$movie->cast}} </div>
