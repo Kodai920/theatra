88b21d650029702ad7dfae54b0018363cdcc1a63
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 13:19:31 +0530

    20191209

--- app/Movie.php
+++ app/Movie.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
+    protected $fillable = ['image'];
+
     public function categories(){
         return $this->belongsToMany('App\Category','movie_category','movie_id','category_id');
     }
