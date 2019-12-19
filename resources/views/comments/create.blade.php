9360fe3a9f2ff9f479e9ae25602bd9f8b6b357fa
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 23:23:38 +0530

    20191209

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="card">
-<div class="card-header">{{$movie->title}}({{$movie->year}}年)のレビュー</div>
+<div class="card-header">のレビュー</div>
 
 <div class="card-body">
     @if(count($errors) > 0)
