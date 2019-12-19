732b0c731b593f47b496fe7f3c891b55a7ff0126
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 13:02:54 +0530

    20191209

--- app/Country.php
+++ app/Country.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    protected $fillable = ['name'];
 
     public function movies(){
         return $this->hasMany('App\Movie');
