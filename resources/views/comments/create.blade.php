88315f1180fe99cd74bfa02afa395f89d47946e4
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 23:06:32 +0530

    20191209

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="card">
-<div class="card-header">{{$movie->title}}({{$movie->year}})のレビュー</div>
+<div class="card-header">{{$movie->title}}({{$movie->year}}年)のレビュー</div>
 
 <div class="card-body">
     @if(count($errors) > 0)
@@ -19,6 +19,10 @@
             <label for="about">感想</label>
             <br>
             <textarea name="about" class="form-control" rows="5"></textarea>
+        </div>
+
+        <div class="range-group form-group">
+            <input type="range" min="1" max="5" value="" class="input-range" />
         </div>
 
         <div class="form-group">
