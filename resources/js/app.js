1469cbf2e69e8034541697a5f29c268e200efb39
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 23:26:16 +0530

    20191209

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,19 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+$(function() {
+    $('.range-group').each(function() {
+      for (var i = 0; i < 5; i ++) {
+        $(this).append('<a>');
+      }
+    });
+    $('.range-group>a').on('click', function() {
+       var index = $(this).index();
+      $(this).siblings().removeClass('on');
+       for (var i = 0; i < index; i++) {
+          $(this).parent().find('a').eq(i).addClass('on');
+       }
+      $(this).parent().find('.input-range').attr('value', index);
+    });
+  });
