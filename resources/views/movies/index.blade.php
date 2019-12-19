4c0c47e1d07a8744092694d626ea38f8bb8066e5
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-19 16:14:37 +0530

    20191209

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
- {{-- <a href="{{route('movies.edit',[$movie->id])}} ">編集</a> --}}
-
     @foreach($movies as $movie)
         <div id="movie-image">
             <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
