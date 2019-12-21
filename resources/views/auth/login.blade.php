d8a049652e12e01b62992b1f07327145438efa0a
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-21 18:21:13 +0530

    20191215

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        {{-- <div class="col-md-8"> --}}
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('ログイン') }}</div>
 
