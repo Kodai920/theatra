ad3a9895610f411670305e20ba741c0f12366b0f
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-21 17:30:54 +0530

    20191215

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -45,7 +45,7 @@ class ReviewController extends Controller
         $review->save();
 
         Session::flash('success','投稿完了');
-        return redirect()->route('movies.show')->with('movie',$movie);
+        return redirect()->route('movies.show')->with('movies',$movies);
     }
 
     /**
