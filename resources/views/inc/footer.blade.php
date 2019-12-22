6f3ce92270c0a348d4e0bab62ef509800d88abbf
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-22 21:32:42 +0530

    20191215

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -8,15 +8,17 @@
         <h5 class="widget-title">Theatra<span></span></h5>
         <p class="para">
             <div>
-                TheatraはNetflixに特化した<br>映画レビューサイトです。
+               <a href="">TheatraはNetflixに特化した<br>映画レビューサイトです。</a>
             </div>
             <div>
             </div>
             <div>
+            <a href="">
                 NETFLIXに関する記事も更新<br>しております。<i class="fas fa-angle-double-down"></i>
                 <li>
                 　<div class="thumb-content"><a href="#">記事一覧</a></div>
                 </li>
+            </a>
             </div>
         </p>
         </div>
