5531733221eca10f4215f935363a834455d6c5ff
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-23 21:16:21 +0530

    20191215

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -16,7 +16,7 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id)
+    public function index()
     {
         Auth::user();
         $profile = Profile::find();
