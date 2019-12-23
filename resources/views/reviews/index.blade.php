2fcca9ef3a5a09a3a8eb61d6b198eac13427b00f
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-23 18:45:17 +0530

    20191215

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="card">
+<div class="card-header">マイページ</div>
+<div class="card-body">
 
+    <div class="wrapper">
+        <div class="row">
+            <div class="5">
+                <div>{{$profile->avator}}</div>
+                <div>{{$user->name}}</div>
+            </div>
+            <div class="5"></div>
+        </div>
+    </div>
 
+</div>
+</div>
 @endsection
