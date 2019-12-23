78bc4bc345ae5ef06e412e713033acbce919225b
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-23 22:49:55 +0530

    20191215

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -14,6 +14,9 @@
             <div class="col-6">
                 <div>
                   <a href="{{route('profiles.index')}}" class="btn btn-secondary btn-block">プロフィール編集</a>
+                  <div class="twitter my-4">
+                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="false">Tweet</a>
+                  </div>
                 </div>
             </div>
         </div>
