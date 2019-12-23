817747d6619f25500f02df6e827772dd49fa7710
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-23 18:17:12 +0530

    20191215

--- resources/views/reviews/create.blade.php
+++ resources/views/reviews/create.blade.php
@@ -56,12 +56,12 @@
                                         <input name="star" id="star-rating-demo" value="0" type="number" class="rating" min=0 max=5 step=0.1 data-size="lg">
                                     </div>
 
-                                    {{-- <div class="twitter my-4">
+                                    <div class="twitter my-4">
                                     　  <a href="//twitter.com/share" class="twitter-share-button" data-text="" data-url="<!-- ここにTweetしたときに入れたいURLを入れる -->" data-lang="ja">
                                         <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                                         Tweet
                                         </a>
-                                    </div> --}}
+                                    </div>
 
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-success btn-block">投稿</button>
