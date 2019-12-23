e54d26c2b14d109f97248835b2c51f81d889001a
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-23 15:52:51 +0530

    20191215

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -41,7 +41,7 @@ class ReviewController extends Controller
     {
         $this->validate($request,[
             'impression' => 'nullable',
-            'star' => 'required',
+            'star' => 'nullable',
         ]);
 
         $movie = Movie::find($id);
