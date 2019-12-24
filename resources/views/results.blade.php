6c1d55d6a6b3f3abdf5026462eb0d985990c9e84
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 22:32:50 +0530

    20191215

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -20,15 +20,9 @@
                         <div class="case-item-wrap">
                             @if(count($movies) > 0)
                             @foreach($movies as $movie)
-                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                                <div class="case-item">
-                                    <div class="case-item__thumb">
                                         <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
                                         <img src="{{asset("uploads/movies/".$movie->image)}}" width="210px" height="300px" alt="{{$movie->title}}">
                                         </a>
-                                    </div>
-                                </div>
-                            </div>
                             @endforeach
                             @else
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
