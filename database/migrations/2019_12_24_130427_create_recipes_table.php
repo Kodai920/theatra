d33540f2c433d8fe8fa409f2f8fb19f3807ce54e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 18:40:35 +0530

    20191215

---
+++ database/migrations/2019_12_24_130427_create_recipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $integer('movie_id')->unsigned()->index();
+            $table->string('name');
+            $table->timestamps();
+
+            //外部キー制約
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
