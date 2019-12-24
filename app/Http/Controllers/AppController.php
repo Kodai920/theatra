bd1fa96df2cf752a2fab2e7f986a06e9ce08224d
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 22:16:10 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -11,8 +11,8 @@ class AppController extends Controller
 {
     public function search(){
         $movies = Movie::where('title','like','%'.request('query').'%')->get();
-        $countries = Country::take(5)->get();
-        $categories = Category::take(5)->get();
+        $countries = Country::where('name','like','%'.request('query').'%')->get();
+        $categories = Category::where('name','like','%'.request('query').'%')->gety();
 
         return view('results')->with('movies',$movies)
                               ->with('title','Search results : '.request('query'))
