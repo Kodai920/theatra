0ae750388a13e2c256473e34617eaefce248b1c8
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 22:33:17 +0530

    20191215

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -16,21 +16,17 @@
     <div class="row medium-padding120">
         <main class="main">
 
-            <div class="row">
-                        <div class="case-item-wrap">
-                            @if(count($movies) > 0)
-                            @foreach($movies as $movie)
-                                        <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
-                                        <img src="{{asset("uploads/movies/".$movie->image)}}" width="210px" height="300px" alt="{{$movie->title}}">
-                                        </a>
-                            @endforeach
-                            @else
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                    <h3 class="text-center">該当する項目はありません。</h3>
-                                </div>
-                            @endif
-                    </div>
-            </div>
+            @if(count($movies) > 0)
+            @foreach($movies as $movie)
+                        <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
+                        <img src="{{asset("uploads/movies/".$movie->image)}}" width="210px" height="300px" alt="{{$movie->title}}">
+                        </a>
+            @endforeach
+            @else
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h3 class="text-center">該当する項目はありません。</h3>
+                </div>
+            @endif
 
             <!-- End Post Details -->
         </main>
