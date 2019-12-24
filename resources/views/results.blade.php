a01fcbdf8f7310759ad967f94466870ca80f86ef
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 22:35:42 +0530

    20191215

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -3,19 +3,12 @@
 
 <!-- Stunning Header -->
 
-<div class="stunning-header stunning-header-bg-lightviolet">
-    <div class="stunning-header-content">
-        <h1 class="stunning-header-title">検索条件 : {{$query}} </h1>
-    </div>
-</div>
+    <h1 class="stunning-header-title">検索条件 : {{$query}} </h1>
 
 <!-- End Stunning Header -->
 <!-- Post Details -->
 
-<div class="container">
-    <div class="row medium-padding120">
         <main class="main">
-
             @if(count($movies) > 0)
             @foreach($movies as $movie)
                         <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
@@ -23,14 +16,9 @@
                         </a>
             @endforeach
             @else
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <h3 class="text-center">該当する項目はありません。</h3>
-                </div>
+                <h3 class="text-center">該当する項目はありません。</h3>
             @endif
-
             <!-- End Post Details -->
         </main>
-    </div>
-</div>
 
 @endsection
