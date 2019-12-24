b9c5b7c0bdd3db6c7d7f785ecc657153e701743f
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 10:34:32 +0530

    20191215

--- public/js/app.js
+++ public/js/app.js
@@ -49601,15 +49601,3 @@ module.exports = __webpack_require__(/*! /Users/okabekodai/Desktop/theatra/resou
 /******/ });
 
 
-$(function() {
-   $('.question_txt').on("click", function() {
-       var id = $(this).data('hoge');
-       $("#toggle_" + id).slideToggle(); 
-       $("#toggle_" + id).toggleClass("active"); 
-       if($(this).next('.show_button').hasClass("closebutton")){
-          $(this).next('.show_button').removeClass("closebutton");
-       }else{
-          $(this).next('.show_button').addClass("closebutton");
-       }
-   });
-});
