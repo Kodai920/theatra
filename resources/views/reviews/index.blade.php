13fc0f38c5c3603e9ea3fba4f150b19dc5068acc
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 11:56:02 +0530

    20191215

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -7,12 +7,12 @@
 
     <div class="wrapper">
         <div class="row">
-            <div class="col-3">
+            <div class="col-6">
                 <div><img src="{{$user->profile->avator}}" alt="" style="height:80px; width:80px;"></div>
-            </div>
-            <div class="col-3">
-                <div style="font-size:14px;">{{$user->name}}</div>
-                <div>登録日 : {{$user->created_at->format('Y年m月d日')}}</div>
+                <div class="float-left">
+                    <div style="font-size:20px;">{{$user->name}}</div>
+                    <div>登録日 : {{$user->created_at->format('Y年m月d日')}}</div>
+                </div>
             </div>
             <div class="col-6">
                 <div>
