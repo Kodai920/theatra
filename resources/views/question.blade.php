0e1c080a4fd4c875c709fccae34d813dc93aff43
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 10:44:45 +0530

    20191215

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="qablock">
+      <div class="qablock">
+        <p class="question_txt" data-hoge="q1"><span class="qicon">Q</span>質問内容をここに記述</p>
+        <div class="show_button closebutton"></div>
+        <div id="toggle_q1" class="answer_txt" style="display:none;">
+          <p>回答内容をここに記述</p>
+        </div>
+      </div>
+
+      <div class="qablock">
+        <p class="question_txt" data-hoge="q2"><span class="qicon">Q</span>質問内容をここに記述</p>
+        <div class="show_button closebutton"></div>
+        <div id="toggle_q2" class="answer_txt" style="display:none;">
+          <p>回答内容をここに記述</p>
+        </div>
+      </div>
+
+      <div class="qablock">
+        <p class="question_txt" data-hoge="q3"><span class="qicon">Q</span>質問内容をここに記述</p>
+        <div class="show_button closebutton"></div>
+        <div id="toggle_q3" class="answer_txt" style="display:none;">
+          <p>回答内容をここに記述</p>
+        </div>
+      </div>
+
+      <div class="qablock">
         <p class="question_txt" data-hoge="q1"><span class="qicon">Q</span>質問内容をここに記述</p>
         <div class="show_button closebutton"></div>
         <div id="toggle_q1" class="answer_txt" style="display:none;">
