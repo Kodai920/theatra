3e8339dddb992a86e84d4aa1a54a2f46e7c049f1
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-24 11:02:17 +0530

    20191215

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -2,10 +2,29 @@
 @section('content')
 
       <div class="qablock">
-        <p class="question_txt" data-hoge="q1"><span class="qicon">Q</span>質問内容をここに記述</p>
+        <p class="question_txt" data-hoge="q1"><span class="qicon">Q</span>ドラマ作品が見つからない</p>
         <div class="show_button closebutton"></div>
         <div id="toggle_q1" class="answer_txt" style="display:none;">
-          <p>回答内容をここに記述</p>
+          <p>
+                レビューを書きたい、 Clip!したいが、ドラマ作品が見つからない場合は下記内容を一度確認またはお試しください。
+
+                <strong>基本情報の確認</strong>
+                □ドラマ作品のタイトルはあっていますか？<br>
+                □キャスト（監督・キャスト）の名前はあっていますか？<br>
+
+                <strong>検索時の入力画面の確認</strong>
+                □ドラマ作品をFilmarks映画版のページで探されていないでしょうか。 映画版でドラマ作品を検索されてもヒットしないため、ドラマ版に切り替えてご確認ください。<br>
+
+                <strong>未登録作品である場合</strong>
+                □上記手順から検索されても作品が見つからない場合、Filmarksに登録されていない作品である可能性がございます。
+                下記フォームより未登録作品情報をお送りください。
+                弊社登録基準を満たしたものを順次登録してまいります。
+                https://forms.gle/yrPHKSWHgj6iY2wD9
+
+                ※リアリティショーやアニメについては現在登録を行なっていないことご了承ください。
+                ※映画への未登録リクエストは現在停止しております。上記フォームから映画情報をお寄せいただきましてもご登録しかねますことご了承ください。
+                ※お寄せいただきましたドラマ情報は弊社登録基準を満たした作品のみ登録をおこないます。
+          </p>
         </div>
       </div>
 
