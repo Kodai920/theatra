549ea6d48064c1d5d5bd14259ca6435038797325
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 10:41:55 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,11 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
-    <form method="GET" action=" {{route('search.results')}}">
+    <form method="GET" action=" {{route('search.results')}}" class="search_container">
         @csrf
         <input class="overlay_search-input" name='query' placeholder="タイトル、カテゴリまたは製作国を入力してください" type="text">
-        <input type="text">
-        {{-- <a href="#" class="overlay_search-close">
-            <span></span>
-            <span></span>
-        </a> --}}
+        <input type="submit" value=""><i class="fas fa-search"></i>
     </form>
 </nav>
