491dedc02d96e3bd8f5b5d45f5dbff8cc442e5c1
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 14:50:23 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -19,8 +19,8 @@ class AppController extends Controller
         return view('results')->with('movies',$movies)
                               ->with('title','Search results : '.request('query'))
                               ->with('name','Search results : '.request('query'))
-                              ->with('query',request('query'));
+                              ->with('query',request('query'))
                             //   ->with('cateogries',$categories)
-                            //   ->with('countries',$countries);
+                              ->with('countries',$countries);
     }
 }
