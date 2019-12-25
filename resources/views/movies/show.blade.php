504be579a8364d5d0f8a2e0453c4a08bd23199a8
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 17:54:53 +0530

    20191215

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -54,14 +54,11 @@
                     <a class="dropdown-item" href="{{route('reviews.edit',[$review->id])}}">
                         編集
                     </a>
-                    <a class="dropdown-item" href="#">
-                        <form action="{{route('reviews.destroy',[$review->id])}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            投稿を削除
-                        </form>
-        
-                    </a>
+                    <form class="dropdown-item" action="{{route('reviews.destroy',[$review->id])}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        投稿を削除
+                    </form>
                 </div>
             </li>
     </div>
