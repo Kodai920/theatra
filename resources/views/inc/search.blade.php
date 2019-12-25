106d109b37819f1399351f7df828e363f95009ea
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 10:37:26 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -2,9 +2,10 @@
     <form method="GET" action=" {{route('search.results')}}">
         @csrf
         <input class="overlay_search-input" name='query' placeholder="タイトル、カテゴリまたは製作国を入力してください" type="text">
-        <a href="#" class="overlay_search-close">
+        <input type="text">
+        {{-- <a href="#" class="overlay_search-close">
             <span></span>
             <span></span>
-        </a>
+        </a> --}}
     </form>
 </nav>
