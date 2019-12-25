65359fce454e28040ece7a9776d9c0096ec080b6
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 11:21:17 +0530

    20191215

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -11,9 +11,11 @@
         <main class="main">
             @if(count($movies) > 0)
             @foreach($movies as $movie)
-                <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
-                <img src="{{asset("uploads/movies/".$movie->image)}}" width="210px" height="300px" alt="{{$movie->title}}">
-                </a>
+                <div id="movie-image">
+                    <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-4">
+                    <img src="{{asset("uploads/movies/".$movie->image)}}" width="210px" height="300px" alt="{{$movie->title}}">
+                    </a>
+                </div>
             @endforeach
             @else
                 <h3 class="text-center">該当する項目はありません。</h3>
