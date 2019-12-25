e4354072c9d079b190fb26ade09cbe14216a022e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 10:34:44 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light" style="background-color:red;">
+<nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <form method="GET" action=" {{route('search.results')}}">
         @csrf
         <input class="overlay_search-input" name='query' placeholder="タイトル、カテゴリまたは製作国を入力してください" type="text">
