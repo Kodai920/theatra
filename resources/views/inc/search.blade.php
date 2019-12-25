fa7290feaadb034cce6cc38780f95ee0182d4317
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 11:13:08 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
-    <form method="GET" action="{{route('search.results')}}" class="search_container mr-auto">
+    <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
         @csrf
         <input name='query' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
         <input type="submit" value="&#xf002">
