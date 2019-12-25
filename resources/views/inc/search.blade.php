93c275da91e213a054ce1577b0bea4c48d824d0c
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 11:03:39 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <form method="GET" action="{{route('search.results')}}" class="search_container">
         @csrf
-        <input name='query' placeholder="タイトル、カテゴリまたは製作国を入力してください"　size="25" type="text">
+        <input name='query' placeholder="キーワードを入力"　size="25" type="text">
         <input type="submit" value="&#xf002">
     </form>
 </nav>
