256750812d876b775c037f39ea6e3c763fba0b22
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-25 16:42:43 +0530

    20191215

--- resources/views/reviews/edit.blade.php
+++ resources/views/reviews/edit.blade.php
@@ -1,8 +1,42 @@
-@extends('layouts.app')
-@section('content')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('', 'Theatra') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ secure_asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.6/css/star-rating.min.css" media="all" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+    <div id="app">
+    @include('inc.navbar')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
 
 <div class="card">
-<div class="card-header">レビュー</div>
+<div class="card-header">レビューの編集</div>
 <div class="card-body">
     @if(count($errors) > 0)
         <ul class="list-group">
@@ -32,4 +66,22 @@
     </form>
 </div>
 </div>
-@endsection
+</div>
+</div>
+</div>
+@include('inc.footer')
+</div>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.6/js/star-rating.min.js" type="text/javascript"></script>
+<script>
+@if(Session::has('success'))
+toastr.success(" {{Session::get('success')}} ")
+@endif
+@if(Session::has('info'))
+toastr.info(" {{Session::get('info')}} ")
+@endif
+</script>
+</body>
+</html>
+
