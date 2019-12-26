cb557d7c45d88eb1628e4f5b2a5d3cadeaf5161e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 17:04:45 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -4,14 +4,14 @@
             @csrf
             <div class="row">
                 <div class="col-md-6 mt-2">
-                    <div class="form-group">
+                    {{-- <div class="form-group">
                         <select name="country_id" class="form-control">
                             <option value="">-Select-</option>
                             @foreach(App\Country::all() as $country)
                                 <option value="{{$country->id}}">{{$country->name}}</option>
                             @endforeach
                         </select>
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="col-md-6 search_container">
                     <input name='query' style="background-color:#E5E5E5" placeholder="キーワードを入力"　size="25" type="text">
