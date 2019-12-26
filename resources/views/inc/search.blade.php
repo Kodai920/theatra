5c0078faa2692cf790f0c22f44fefae2fc78e4d0
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 11:37:31 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -5,7 +5,7 @@
         <input class="mx-1" type="radio" name="search-box" id=""> カテゴリ
         <input class="mx-1" type="radio" name="search-box" id=""> 作成国
     </form>
-    <form method="GET" action="{{route('search.results')}}" class="search_container ml-2">
+    <form method="GET" action="{{route('search.results')}}" class="search_container ml-3">
         @csrf
         <input name='title' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
         <input type="submit" value="&#xf002">
