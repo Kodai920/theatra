742363bb1e09af8573f00aa66e1645211aae7e1e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 21:59:37 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <div class="container">
-    <form method="GET" action="{{route('search.results')}}" class="ml-auto">
+    <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
         @csrf
-        <div class="float-left mt-2">
-            <label><input class="mx-1" type="radio" name="search" value="{{$movie}}" checked>タイトル</label>
-            <label><input class="mx-1" type="radio" name="search" value="{{$category}}">カテゴリ</label>
-            <label><input class="mx-1" type="radio" name="search" value="{{$country}}">製作国</label>
-        </div>
-        <div class="ml-3 float-left search_container">
+        {{-- <div class="float-left mt-2">
+            <label><input class="mx-1" type="radio" name="search" value="movie" checked>タイトル</label>
+            <label><input class="mx-1" type="radio" name="search" value="category">カテゴリ</label>
+            <label><input class="mx-1" type="radio" name="search" value="">製作国</label>
+        </div> --}}
+        {{-- <div class="ml-3 float-left search_container"> --}}
             <input name='title' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
             <input type="submit" value="&#xf002">
-        </div>
+        {{-- </div> --}}
     </form>
     </div>
 </nav>
