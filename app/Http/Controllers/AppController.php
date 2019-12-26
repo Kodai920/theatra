aa61283dd500bb02a9850dcb681677ddccd3ad01
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 20:42:55 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -10,15 +10,15 @@ use App\Country;
 class AppController extends Controller
 {
     public function search(){
-        $movies = Movie::where('title','like','%'.request('query').'%');
+        $movies = Movie::where('title','like','%'.request('query').'%')->get();
 
         $movies = Movie::whereHas('country',function($query){
                           $query->where('name','like','%'.request('query').'%');
-                        });
+                        })->get();
 
-        // $movies = Movie::whereHas('categories',function($query){
-        //                    $query->where('name','like','%'.request('query').'%');
-        //                  })->get();
+        $movies = Movie::whereHas('categories',function($query){
+                           $query->where('name','like','%'.request('query').'%');
+                         })->get();
 
         return view('results')->with('movies',$movies)
                               ->with('title','Search results : '.request('query'))
