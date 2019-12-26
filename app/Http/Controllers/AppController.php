2a54d3f8a00c56edc49aafc350e25a514fe9739b
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 11:21:44 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -9,14 +9,31 @@ use App\Country;
 
 class AppController extends Controller
 {
-    public function search(){
-        $movies = Movie::where('title','like','%'.request('query').'%')->get();
+    public function search(Request $request){
+
+      $movie_name = $request->get('title');
+      $category_name = $request->get('category_id');
+      $country = $request->get('country_id');
+
+
+      if( $movie_name || $category_name || $country){
+
+        $movies = Movie::where('title','like','%'.$movie_name.'%')
+                  ->orWhere('category', $category_name)
+                  ->orWhere('country',$country);
 
         return view('results')->with('movies',$movies)
-                              ->with('title','Search results : '.request('query'))
-                              ->with('name','Search results : '.request('query'))
-                              ->with('query',request('query'));
-                              // ->with('cateogries',$categories)
-                              // ->with('country',$country);
+                  ->with('title','Search results : '.request('query'))
+                  ->with('name','Search results : '.request('query'))
+                  ->with('query',request('query'))
+                   ->with('category',$category_name)
+                   ->with('country',$country);
+      }
+      else{
+        return view('movies.index')->with('movies',Movie::all());
+      }
+
+
     }
+
 }
--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,8 +1,13 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <div class="container">
+        <form action="" method="POST">
+            <input type="radio" name="search-box" id="">タイトル
+            <input type="radio" name="search-box" id="">カテゴリ
+            <input type="radio" name="search-box" id="">作成国
+        </form>
     <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
         @csrf
-        <input name='query' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
+        <input name='title' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
         <input type="submit" value="&#xf002">
     </form>
     </div>
