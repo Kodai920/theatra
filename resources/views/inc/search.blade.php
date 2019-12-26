8af1d6767133e9dc5e7ddcfeeeb51ef3aa9d0ad3
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 15:28:33 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -4,9 +4,9 @@
             @csrf
             <div class="row">
                 <div class="col-md-6 mt-2">
-                    <input class="mx-1" type="radio" value="mr" name="search" checked>タイトル
-                    <input class="mx-1" type="radio" value="cr" name="search">カテゴリ
-                    <input class="mx-1" type="radio" value="conr" name="search">作成国
+                    <input class="mx-1" type="radio" name="mr" name="search" checked>タイトル
+                    <input class="mx-1" type="radio" name="cr" name="search">カテゴリ
+                    <input class="mx-1" type="radio" name="conr" name="search">作成国
                 </div>
                 <div class="col-md-6 search_container">
                     <input name='query' style="background-color:#E5E5E5" placeholder="キーワードを入力"　size="25" type="text">
