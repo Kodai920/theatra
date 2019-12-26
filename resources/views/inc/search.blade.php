af4af00bf9ae82023fc608a7daf9cfe41846b8e4
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 12:03:54 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -3,7 +3,7 @@
         <form method="GET" action="{{route('search.results')}}" class="ml-auto">
             @csrf
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-6 mt-2">
                     <input class="mx-1" type="radio" name="search-box">タイトル
                     <input class="mx-1" type="radio" name="search-box">カテゴリ
                     <input class="mx-1" type="radio" name="search-box">作成国
