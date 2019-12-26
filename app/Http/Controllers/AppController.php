0ec077f899d19ac238c3ed9284932770174b2801
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 22:10:02 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -11,34 +11,17 @@ class AppController extends Controller
 {
     public function search(){
 
-      // $movie = $request->get('title');
-      // $category = $request->get('name');
-      // $country = $request->get('name');
-
-      // if($movie||$country||$category){
-
-      //   $movies = Movie::query();
-
-      //   if($movie){
           $movies = Movie::where('title','like','%'.request('query').'%')
-        // }
-
-        // if($category){
           ->whereHas('categories',function($query){
             $query->where('name','like','%'.request('query').'%');
           })
-        // }
-
-        // if($country){
           ->whereHas('country',function($query){
             $query->where('name','like','%'.request('query').'%');
           })->get();
-    //     }
 
         return view('results')->with('movies',$movies)
         ->with('title','Search results : '.request('query'))
         ->with('name','Search results : '.request('query'))
         ->with('query',request('query'));
-    //   }
     }
 }
