223f3e08c32f31d9086d7d21a58aa348e2d8136e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 22:37:47 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,9 +1,16 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <div class="container">
-    <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
+    <form method="GET" action="{{route('search.results')}}" class="ml-auto">
         @csrf
-        <input name='query' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
-        <input type="submit" value="&#xf002">
+        <div class="mt-2">
+            <input class="mx-1" type="radio" name="search" id="">タイトル
+            <input class="mx-1" type="radio" name="search" id="">カテゴリ
+            <input class="mx-1" type="radio" name="search" id=""> 作成国
+        </div>
+        <div class="search_container ml-3">
+            <input name='query' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
+            <input type="submit" value="&#xf002">
+        </div>
     </form>
     </div>
 </nav>
