0f9bb590cb77c2f3d2f4ef82da5e15ed1cba8387
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 21:05:32 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -2,12 +2,12 @@
     <div class="container">
     <form method="GET" action="{{route('search.results')}}" class="ml-auto">
         @csrf
-        <div class="float-left">
+        <div class="float-left mt-2">
             <input class="mx-1" type="radio" name="search-box" >タイトル
             <input class="mx-1" type="radio" name="search-box" >カテゴリ
             <input class="mx-1" type="radio" name="search-box" >作成国
         </div>
-        <div class="ml-3 search_container">
+        <div class="ml-3 float-left search_container">
             <input name='title' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
             <input type="submit" value="&#xf002">
         </div>
