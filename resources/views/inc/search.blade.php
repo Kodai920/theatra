e6a1d8203fad5843c02ad42130f7a8552cbb1b5e
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 11:58:02 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,16 +1,18 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <div class="container">
-    <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
-        @csrf
-        <div class="">
-            <input class="mx-1" type="radio" name="search-box">タイトル
-            <input class="mx-1" type="radio" name="search-box">カテゴリ
-            <input class="mx-1" type="radio" name="search-box">作成国
-        </div>
-        <div class="ml-3">
-            <input name='title' style="background-color:#E5E5E5;" placeholder="キーワードを入力"　size="25" type="text">
-            <input type="submit" value="&#xf002">
-        </div>
-    </form>
+        <form method="GET" action="{{route('search.results')}}" class="search_container ml-auto">
+            @csrf
+            <div class="row">
+                <div class="col-md-8">
+                    <input class="mx-1" type="radio" name="search-box">タイトル
+                    <input class="mx-1" type="radio" name="search-box">カテゴリ
+                    <input class="mx-1" type="radio" name="search-box">作成国
+                </div>
+                <div class="col-md-4">
+                    <input name='title'  placeholder="キーワードを入力"　size="25" type="text">
+                    <input type="submit" value="&#xf002">
+                </div>
+            </div>
+        </form>
     </div>
 </nav>
