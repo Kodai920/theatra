c3c8bcb48e7e0b1ca7f05075398ef39a71d7bb13
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 11:25:37 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light" style="background-color:#616161;">
     <div class="container">
-    <form action="" method="POST" class="float-right">
+    <form action="" method="POST" class="ml-auto">
         <input class="mx-1" type="radio" name="search-box" id=""> タイトル
         <input class="mx-1" type="radio" name="search-box" id=""> カテゴリ
         <input class="mx-1" type="radio" name="search-box" id=""> 作成国
