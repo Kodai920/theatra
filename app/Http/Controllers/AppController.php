0569a60507d6354b62e0aa141d61405aa22a79a1
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-26 21:49:33 +0530

    20191215

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -11,34 +11,34 @@ class AppController extends Controller
 {
     public function search(Request $request){
 
-      $movie = $request->get('title');
-      $category = $request->get('name');
-      $country = $request->get('name');
+      // $movie = $request->get('title');
+      // $category = $request->get('name');
+      // $country = $request->get('name');
 
-      if($movie||$country||$category){
+      // if($movie||$country||$category){
 
-        $movies = Movie::query();
+      //   $movies = Movie::query();
 
-        if($movie){
+      //   if($movie){
           $movies = Movie::where('title','like','%'.request('query').'%')->get();
-        }
+        // }
 
-        if($category){
+        // if($category){
           $movies = Movie::whereHas('categories',function($query){
             $query->where('name','like','%'.request('query').'%');
           })->get();
-        }
+        // }
 
-        if($country){
+        // if($country){
           $movies = Movie::whereHas('country',function($query){
             $query->where('name','like','%'.request('query').'%');
           })->get();
-        }
+    //     }
 
-        return view('results')->with('movies',$movies)
-        ->with('title','Search results : '.request('query'))
-        ->with('name','Search results : '.request('query'))
-        ->with('query',request('query'));
-      }
+    //     return view('results')->with('movies',$movies)
+    //     ->with('title','Search results : '.request('query'))
+    //     ->with('name','Search results : '.request('query'))
+    //     ->with('query',request('query'));
+    //   }
     }
 }
