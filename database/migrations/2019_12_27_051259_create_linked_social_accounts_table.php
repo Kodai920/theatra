06648c035deb67551831aa0e7acd0db80ef74dd8
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-27 10:43:36 +0530

    20191215

--- database/migrations/2019_12_27_051259_create_linked_social_accounts_table.php
+++ database/migrations/2019_12_27_051259_create_linked_social_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLinkedSocilAccountsTable extends Migration
+class CreateLinkedSocialAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLinkedSocilAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('linked_socil_accounts', function (Blueprint $table) {
+        Schema::create('linked_social_accounts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id');
             $table->string('provider_name')->nullable();
@@ -29,6 +29,6 @@ class CreateLinkedSocilAccountsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('linked_socil_accounts');
+        Schema::dropIfExists('linked_social_accounts');
     }
 }
