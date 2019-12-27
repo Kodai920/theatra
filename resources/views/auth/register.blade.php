3752dc7e93902cffe49466d25f7b66d097be6a90
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-27 17:55:57 +0530

    20191215

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -69,11 +69,11 @@
                         </div>
 
                         <hr>
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 justify-content-center">
                             <div class="col-8">
-                                <a href="{{ url('/login/twitter') }}" class="btn btn-twitter float-left"><i class="fa fa-twitter"></i> Twitter</a>
-                                <a href="{{ url('/login/facebook') }}" class="btn btn-facebook float-left"><i class="fa fa-facebook"></i> Facebook</a>
-                                <a href="{{ url('/login/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
+                                <a href="{{ url('/login/twitter') }}" class="btn btn-twitter col-md-3"><i class="fa fa-twitter"></i> Twitter</a>
+                                <a href="{{ url('/login/facebook') }}" class="btn btn-facebook col-md-3"><i class="fa fa-facebook"></i> Facebook</a>
+                                <a href="{{ url('/login/github') }}" class="btn btn-github col-md-3"><i class="fa fa-github"></i> Github</a>
                             </div>
                         </div>
 
