1ef6aeb9d545999a150c1cbd0e770faa838d149d
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-27 17:52:55 +0530

    20191215

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -67,14 +67,14 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
-                    <hr>
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        <div class="mx-auto mb-0">
+
+                        <hr>
+                        <div class="form-group mx-auto mb-0">
                             <a href="{{ url('/login/twitter') }}" class="btn btn-twitter float-left"><i class="fa fa-twitter"></i> Twitter</a>
                             <a href="{{ url('/login/facebook') }}" class="btn btn-facebook float-left"><i class="fa fa-facebook"></i> Facebook</a>
                             <a href="{{ url('/login/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
                         </div>
+
                     </form>
                 </div>
             </div>
