494c50c861f6fc738b09cd71147aa353a1730ffe
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-27 15:32:29 +0530

    20191215

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -3,11 +3,6 @@
     <form method="GET" action="{{route('search.results')}}" class="ml-auto">
         @csrf
         <div class="float-left mt-1 mr-2">
-            {{-- <select name="searchby">
-                <option value="movie" selected>タイトル</option>
-                <option value="category">カテゴリ</option>
-                <option value="country">製作国</option>
-            </select> --}}
             <input type="radio" name="searchby" id="" value="movie" class="mr-1 ml-2" checked>タイトル
             <input type="radio" name="searchby" id="" value="category" class="mr-1 ml-2">カテゴリ
             <input type="radio" name="searchby" id="" value="country" class="mr-1 ml-2">製作国
