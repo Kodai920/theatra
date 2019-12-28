b352a2bf142c393c1ab36f3a8ef169b5f2aefb92
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-28 21:34:36 +0530

    20191215

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -60,6 +60,7 @@ class MovieController extends Controller
      */
     public function show(Movie $movie)
     {
+        $userId = Auth::id();//ログイン中のid
         $category = Category::all();
         $country = Country::all();
         return view('movies.show')->with('movie',$movie)
