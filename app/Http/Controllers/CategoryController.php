6a350b2f4c9714867941b0f5b3371573a6d00e07
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-28 18:05:49 +0530

    20191215

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -57,7 +57,7 @@ class CategoryController extends Controller
     {
         $movies = Movie::all();
         return view('categories.show')->with('category',$category)
-                                      ->with('movie',$movie);
+                                      ->with('movies',$movies);
     }
 
     /**
