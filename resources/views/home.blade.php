7c976fb6d253aae4e7be853f5579c6aeb1b79a3d
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-29 17:54:24 +0530

    20191215

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
                 <div class="col-4">
                     <div id="movie-image">
                     <a href="{{route('categories.show',['category'=>41])}}">
-                    <img class="d-block mx-auto" src="uploads/homes/action.jpg" width="300px" height="210px" alt="netflix">
+                    <img class="d-block mx-auto" src="uploads/homes/action.jpg" width="300px" height="210px" alt="action">
                     </a>
                     </div>
                     <h5 class="text-center mt-2">アクション</h5>
@@ -89,7 +89,7 @@
                 <div class="col-4">
                     <div id="movie-image">
                     <a href="{{route('categories.show',['category'=>251])}}">
-                    <img class="d-block mx-auto" src="uploads/homes/love.jpg" width="300px" height="210px" alt="netflix">
+                    <img class="d-block mx-auto" src="uploads/homes/love.jpg" width="300px" height="210px" alt="love">
                     </a>
                     </div>
                     <h5 class="text-center mt-2">ラブコメディ</h5>
@@ -100,7 +100,7 @@
                 <div class="col-4">
                     <div id="movie-image">
                     <a href="{{route('categories.show',['category'=>361])}}">
-                    <img class="d-block mx-auto" src="uploads/homes/horror.jpg" width="300px" height="210px" alt="netflix">
+                    <img class="d-block mx-auto" src="uploads/homes/horror.jpg" width="300px" height="210px" alt="horror">
                     </a>
                     </div>
                     <h5 class="text-center mt-2">ホラー</h5>
@@ -108,7 +108,7 @@
                 <div class="col-4">
                     <div id="movie-image">
                     <a href="{{route('categories.show',['category'=>51])}}">
-                    <img class="d-block mx-auto" src="uploads/homes/humandrama.jpg" width="300px" height="210px" alt="netflix">
+                    <img class="d-block mx-auto" src="uploads/homes/humandrama.jpg" width="300px" height="210px" alt="humandrama">
                     </a>
                     </div>
                     <h5 class="text-center mt-2">ヒューマンドラマ</h5>
@@ -116,7 +116,7 @@
                 <div class="col-4">
                     <div id="movie-image">
                     <a href="{{route('categories.show',['category'=>241])}}">
-                    <img class="d-block mx-auto" src="uploads/homes/comedy.jpg" width="300px" height="210px" alt="netflix">
+                    <img class="d-block mx-auto" src="uploads/homes/comedy.jpg" width="300px" height="210px" alt="comedy">
                     </a>
                     </div>
                     <h5 class="text-center mt-2">コメディ</h5>
@@ -132,7 +132,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>21])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/japan.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/japan.jpg" width="300px" height="210px" alt="japan">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">日本</h5>
@@ -140,7 +140,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>1])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/us.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/us.jpg" width="300px" height="210px" alt="us">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">アメリカ</h5>
@@ -148,7 +148,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>11])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/uk.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/uk.jpg" width="300px" height="210px" alt="uk">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">イギリス</h5>
@@ -159,7 +159,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>41])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/france.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/france.jpg" width="300px" height="210px" alt="france">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">フランス</h5>
@@ -167,7 +167,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>31])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/india.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/india.jpg" width="300px" height="210px" alt="india">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">インド</h5>
@@ -175,7 +175,7 @@
                     <div class="col-4">
                         <div id="movie-image">
                         <a href="{{route('countries.show',['country'=>51])}}">
-                        <img class="d-block mx-auto" src="uploads/homes/korea.jpg" width="300px" height="210px" alt="netflix">
+                        <img class="d-block mx-auto" src="uploads/homes/korea.jpg" width="300px" height="210px" alt="korea">
                         </a>
                         </div>
                         <h5 class="text-center mt-2">韓国</h5>
