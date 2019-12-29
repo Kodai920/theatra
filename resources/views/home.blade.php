7aadeead62cfa8ad82cd505130488c61e4d4cc7f
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-29 12:41:38 +0530

    20191215

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,8 +52,7 @@
             @endif
             @endforeach
             </div><br>
-            <a href="{{route('movies.index')}}" class="btn btn-block btn-outline-dark" style="background-color:#E40914;">すべての映画を見る</a>
-        </div>
+        </div><br>
         <hr color="#131313">
 
         <div class="my-5">
