cba342b741df51a35191bb34dc2b757fc2281b3a
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-30 12:33:32 +0530

    20191215

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -10,14 +10,10 @@
             </div>
             <div class="col-7">
                 <ul class="mt-3 pl-0 movie-detail-info">
-                    <li class="mb-2"><strong>制作国 : </strong><a href="{{route('countreies.show',[$country->id])}}
-                        {{-- /countries/{$country->id} --}}
-                        ">{{$movie->country->name}}</a></li>
+                    <li class="mb-2"><strong>制作国 : </strong><a href="#">{{$movie->country->name}}</a></li>
                     <li class="mb-2"><strong>カテゴリ : </strong>
                         @foreach($movie->categories as $category)
-                        <a href="
-                        {{-- /categories/{$cateogry->id} --}}
-                        ">
+                        <a href="#">
                         　{{$category->name}}
                         </a>
                         @endforeach
