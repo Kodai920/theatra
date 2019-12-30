5c05c8b9591caa68844c5a5091175604a789cf17
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-30 10:36:04 +0530

    20191215

--- database/migrations/2019_12_30_050213_add_admin_to_users_table.php
+++ database/migrations/2019_12_30_050213_add_admin_to_users_table.php
@@ -14,7 +14,7 @@ class AddAdminToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->booleam('admin')->default(0);
+            $table->boolean('admin')->default(0);
         });
     }
 
