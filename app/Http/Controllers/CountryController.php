4ecd2502c2b7315cfebc27cb2f21bf2d0e977d2a
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-01 16:44:27 +0530

    20191215

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -18,9 +18,7 @@ class CountryController extends Controller
     {
         $countries = Country::all();
         $movie = Movie::all();
-        $count = Movie::where('country_id',$countries->movie->country_id)->count();
         return view('countries.index')->with('countries',$countries)
-                                      ->with('count',$count)
                                       ->with('movie',$movie);
     }
 
