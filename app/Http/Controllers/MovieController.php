f72ad05d1d13affacdddd21f968ef0dca37058d7
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-01 15:49:20 +0530

    20191215

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -28,7 +28,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::all()->paginate(16);
         return view('movies.index')->with('movies',$movies);
     }
 
