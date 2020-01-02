abf3ca05ad17046e1c7fc43e5f142fa9f8c186fd
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-02 16:04:26 +0530

    20191215

--- app/Movie.php
+++ app/Movie.php
@@ -9,6 +9,8 @@ class Movie extends Model
 {
     protected $fillable = ['title','year','about','image'];
 
+    use Rateable;
+
     public function categories(){
         return $this->belongsToMany('App\Category');
     }
