cc7d4a0e51a3338498cf725e96912412a571f76d
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-02 21:30:55 +0530

    20191215

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class RankController extends Controller
 {
+
     public function index(){
-        return view('rank');
+
+        $movies = Movie::all();
+        $reviews = Reviews::all();
+
+        return view('rank')->with('movie',$movie)
+                           ->with('review',$review);
     }
 }
