ce7340a6ee4b7c0eb1d1da9ee818280a0c735345
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-02 20:53:14 +0530

    20191215

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
 
-
+<div class="cp_tab">
+        <div class="cp_tabpanels">
+            <label for="tab5_1">First Tab</label>
+            <input id="tab5_1" name="cp_tab" type="radio" checked="checked">
+            <div class="cp_tabpanel">
+                <h4>First Tab</h4>
+                <p>First Tab text</p>
+            </div>
+        </div>
+        <div class="cp_tabpanels">
+            <label for="tab5_2">Second Tab</label>
+            <input id="tab5_2" name="cp_tab" type="radio">
+            <div class="cp_tabpanel">
+                <h4>Second Tab</h4>
+                <p>Second Tab text</p>
+            </div>
+        </div>
+    </div>
 
 @endsection
