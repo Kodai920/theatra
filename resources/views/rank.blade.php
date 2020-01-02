37c48dd6a6d838e4da51f28cfb02f16ada59a117
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-02 21:06:13 +0530

    20191215

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="cp_tab card">
+<div class="cp_tab">
         <div class="cp_tabpanels">
             <label for="tab5_1">Theatraで評価が高い作品</label>
             <input id="tab5_1" name="cp_tab" type="radio" checked="checked">
-            <div class="cp_tabpanel">
+            <div class="cp_tabpanel card">
                 <h4 class="card-header">評価ランキング</h4>
                 <div class="card-body"></div>
             </div>
@@ -13,7 +13,7 @@
         <div class="cp_tabpanels">
             <label for="tab5_2">投稿数が多い作品</label>
             <input id="tab5_2" name="cp_tab" type="radio">
-            <div class="cp_tabpanel">
+            <div class="cp_tabpanel card">
                 <h4 class="card-header">投稿数ランキング</h4>
                 <div class="card-body"></div>
             </div>
