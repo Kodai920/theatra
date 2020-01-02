687e40177b04318578109f2c6dbd64153127cd3a
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-02 15:31:07 +0530

    20191215

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        willvincent\Rateable\RateableServiceProvider::class,
 
         /*
          * Package Service Providers...
