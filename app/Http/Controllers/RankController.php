24fabcd3e7dc56b5e2c488ad1695709ca06d69fc
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-03 12:45:01 +0530

    20191215

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -14,7 +14,7 @@ class RankController extends Controller
 
         $movies = Movie::with('reviews')->get()->sortBy(function($movie){
             return $movie->reviews->count();
-        });
+        },SORT_REGULAR,true);
         $reviews = Review::all();
 
         return view('rank')->with('movies',$movies)
