8f8f4ac9dcc403a53e5bdcad035954192b30fe23
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-06 21:08:55 +0530

    20191215

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -35,11 +35,11 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
-                @if(Auth::user()->admin)
+                {{-- @if(Auth::user()->admin) --}}
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('manage')}}">管理画面</a>
                 </li>
-                @endif
+                {{-- @endif --}}
                 @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
