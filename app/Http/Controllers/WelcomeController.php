1c4318cddf57393d38c9c97224da7617377f7311
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-07 23:23:44 +0530

    20200108

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+        /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('welcome')->with('movies',$movies);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Auth::routes();
+
+Route::get('/', 'WelcomeController@index')->name('welcome');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
