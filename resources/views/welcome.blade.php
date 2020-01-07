4380cb9ff4389eb3732d3dd5d0fb9ff4cef4b980
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-08 00:38:39 +0530

    20200108

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
             @if($loop->index <= 8)
                 <div id="movie-image">
                     <a href="{{route('movies.show',[$movie->id])}}" class="float-left m-3">
-                    <img src="{{asset("uploads/movies/".$movie->image)}}" width="120vw" height="180vw" alt="{{$movie->title}}">
+                    <img src="{{asset("uploads/movies/".$movie->image)}}" class="welcome-movie" alt="{{$movie->title}}">
                     </a>
                 </div>
             @endif
