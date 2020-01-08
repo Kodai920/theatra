cbd098815840a255728ed8df19c2d5e979814959
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-08 21:33:49 +0530

    20200108

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -11,7 +11,8 @@ use App\Review;
 class MemberController extends Controller
 {
     public function index(){
-        $user = Auth::user();
+
+        $user = User::all();
         $reviews = Review::all();
         return view('member')->with('user',$user)
                              ->with('reviews',$reviews);
