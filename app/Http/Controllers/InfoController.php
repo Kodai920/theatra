eef806f3d4ac1bf16a2ebc033c26d04a2d81faee
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-08 21:45:08 +0530

    20200108

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
+use App\Review;
+use App\Country;
 
 class InfoController extends Controller
 {
     public function index(){
-        return view('info');
+        $movies = Movie::all();
+        $reviews = Review::all();
+        $country = Country::all();
+        return view('info')->with('movies',$movies)
+                           ->with('reviews',$reviews)
+                           ->with('country',$country);
     }
 }
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -5,8 +5,32 @@
         <div class="card-header">映画情報</div>
 
         <div class="card-body">
+                <table class="rank-color mx-auto" width="100%">
+                    <tr class="rank-header">
+                        <th>No.</th>
+                        <th>タイトル</th>
+                        <th>公開年</th>
+                        <th>製作国</th>
+                        <th>投稿数</th>
+                        <th>削除</th>
+                    </tr>
+                    <?php $i=0; ?>
+                    @foreach($movies as $info)
+                    <?php $i++; ?>
+                    <tr>
+                        <td><strong>{{$i}}</strong></td>
+                        <td>
+                            {{$info->name}}
+                        </td>
+                        <td>{{$info->year}}年</td>
+                        <td>{{$info->country->name}}</td>
+                        <td>{{$info->reviews->count()}}</td>
+                        <td>削除</td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
-</div>
 
 
 @endsection
