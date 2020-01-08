140e162fb6b3a2a2e3268d67d112df8d50c0b4d4
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-08 21:01:44 +0530

    20200108

---
+++ resources/views/info.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.page')
+@section('page')
+
+<div class="card">
+        <div class="card-header">映画情報</div>
+
+        <div class="card-body">
+        </div>
+</div>
+
+
+@endsection
