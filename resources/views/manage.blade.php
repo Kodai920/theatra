7430e07097341524918f71a58fcb1a88d91acb29
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-09 21:15:04 +0530

    20200108

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -5,11 +5,11 @@
         <div class="card-header">ダッシュボード</div>
 
         <div class="card-body">
-          <div class="row">
-            <div class="col-md-4 text-center">会員数 : {{$user->count()}}</div>
-            <div class="col-md-4 text-center">投稿数 : {{$reviews->count()}}</div>
-            <div class="col-md-4 text-center">映画数 : {{$movies->count()}}</div>
-          </div>
+                <div class="row">
+                <div class="col-md-4 text-center rounded">会員数 : {{$user->count()}}</div>
+                <div class="col-md-4 text-center rounded">投稿数 : {{$reviews->count()}}</div>
+                <div class="col-md-4 text-center rounded">映画数 : {{$movies->count()}}</div>
+                </div>
         </div>
 </div>
 
