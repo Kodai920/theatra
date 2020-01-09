d64c52ad92ae351efea0f90ca555d142b427993c
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-09 21:01:35 +0530

    20200108

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -5,6 +5,11 @@
         <div class="card-header">ダッシュボード</div>
 
         <div class="card-body">
+          <div class="row">
+            <div class="col-md-4">会員数</div>
+            <div class="col-md-4">投稿数</div>
+            <div class="col-md-4">映画数</div>
+          </div>
         </div>
 </div>
 
