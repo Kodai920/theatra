79b4ad343c2619460603cc61857f3df8d1e08630
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 21:10:32 +0530

    20200108

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,11 +9,11 @@
           {{$post->created_at->format('Y/m/d')}}
         </div>
         <div class="text-center">
-          <h3>{{$post->title}}</h3>
+          <h2>{{$post->title}}</h2>
         </div>
-        <div style="width:100%; height:100px;" class="my-2">
-          <img src="{{asset("uploads/posts/".$post->featured_img)}}">
-        </div>
+        <div class="my-2">
+          <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="80%" height="100px">
+        </div><br>
         <div>
             {{$post->excerpt}}
         </div>
