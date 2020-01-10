195863d965015d6ee14a6df846de0aabaf4f5e14
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 20:47:13 +0530

    20200108

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
 <div>
 <div class="card">
     <div class="card-body">
-            {{$post->created_at->format(Y/m/d)}}
+            {{$post->created_at->format(y/m/d)}}
             {{$post->title}}
             <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="80px" height="80px"></td>
             {{$post->about}}
