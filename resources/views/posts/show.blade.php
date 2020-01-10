acc0520ef3d2fb65675aafe76aba8b59c4bf4001
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 21:52:00 +0530

    20200108

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,16 @@
 <div class="card">
     <div class="card-body">
         <div class="text-center">
-            {{$posts->created_at('Y/m/d')}}
+            {{$post->created_at('Y/m/d')}}
         </div>
         <div class="textcenter">
-            {{$posts->title}}
+            {{$post->title}}
         </div>
         <div class="">
             <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="100%" height="150px">
         </div>
         <div>
-            {{$posts->about}}
+            {{$post->about}}
         </div>
     </div>
 </div>
