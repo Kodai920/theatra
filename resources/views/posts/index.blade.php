65c8c7f68aa5146ea31815ff7d5b362362826166
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 21:23:21 +0530

    20200108

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 
 @foreach($posts as $post)
-<div class="row">
+<div class="row m-2">
 <div class="card col-md-6">
     <div class="card-body">
         <div class="text-center" style="color:gray;">
           {{$post->created_at->format('Y/m/d')}}
         </div>
         <div class="text-center">
-          <h2>{{$post->title}}</h2>
+          <h3>{{$post->title}}</h3>
         </div>
         <div class="my-2">
           <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="100%" height="150px">
@@ -17,8 +17,8 @@
         <div>
             {{$post->excerpt}}
         </div>
-        <div class="mx-auto my-2">
-          <a href="" class="btn btn-secondary text-center">Read More</a>
+        <div class="text-center">
+          <a href="#" class="btn btn-secondary text-center">Read More</a>
         </div>
     </div>
 </div>
