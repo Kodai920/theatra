2ec279fdd1c0ad230ce7eb212999b5ced4281588
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 20:40:53 +0530

    20200108

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,11 @@
 @foreach($posts as $post)
 <div class="card">
     <div class="card-body">
-            <td><img src="{{asset("uploads/posts/".$post->featured_img)}}" width="80px" height="80px"></td>
-            <td>
-                {{$post->title}}
-            </td>
+            {{$post->created_at->format(yyyy/mm/dd)}}
+            {{$post->title}}
+            <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="80px" height="80px"></td>
+
+            {{$post->title}}
     </div>
 </div>
 @endforeach
