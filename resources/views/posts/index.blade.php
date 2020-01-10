f3d9be2a0673839252bd640f9682fc6164843435
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 21:01:31 +0530

    20200108

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,11 +11,14 @@
         <div class="text-center">
           <h3>{{$post->title}}</h3>
         </div>
-        <div style="width:100%; height:120px;">
+        <div style="width:100%; height:100px;">
           <img src="{{asset("uploads/posts/".$post->featured_img)}}">
         </div>
         <div>
-          {{$post->about}}
+            {{$post->excerpt}}
+        </div>
+        <div>
+            <a href="" class="btn btn-light text-center mx-auto">Read More</a>
         </div>
     </div>
 </div>
