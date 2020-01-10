13ab9ae9595595e47cffa0c1f65abcb3c8098d34
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 22:09:04 +0530

    20200108

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,7 @@
 <div class="card">
     <div class="card-body">
         <div class="text-center">
+            <a href="{{route('posts.index')}}" class="mr-auto btn btn-lg btn-light text-center border border-dark">Back</a>
             {{$post->created_at->format('Y/m/d')}}
         </div>
         <div class="text-center my-3">
@@ -14,6 +15,9 @@
         </div>
         <div class="my-5">
             {{$post->about}}
+        </div>
+        <div class="text-center my-2">
+            <a href="{{route('posts.index')}}" class="btn btn-lg btn-light text-center border border-dark">Back</a>
         </div>
     </div>
 </div>
