0e4105659323f81e99ba277921333c313012cc2d
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-10 22:35:17 +0530

    20200108

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,7 @@
         <div class="text-center">
             {{$post->created_at->format('Y/m/d')}}
         </div>
-        <div class="text-center mb-2 mt-5">
+        <div class="text-center mt-4">
             <h2>{{$post->title}}</h2>
         </div>
         <div class="my-5">
