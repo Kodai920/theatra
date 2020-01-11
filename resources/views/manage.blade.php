ca62474c0cacf45683b9b3dda5217c9e7872c0b0
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-11 17:52:10 +0530

    20200108

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.page')
 @section('page')
 
-<div class="card">
+<div class="card my-3">
         <div class="card-header">ダッシュボード</div>
 
         <div class="card-body">
@@ -15,6 +15,39 @@
                 </div>
         </div>
 </div>
+
+<div class="card">
+        <div class="card-header">最近の投稿</div>
+        <div class="card-body">
+                <div class="row">
+                        @foreach($posts->sortByDesc('created_at') as $post)
+                        @if($loop->index <= 1)
+                        <div class="card col-md-5 mx-4 my-3">
+                                <div class="card-body">
+                                <div class="text-center" style="color:gray;">
+                                        {{$post->created_at->format('Y/m/d')}}
+                                </div>
+                                <div class="text-center my-3">
+                                        <h3>{{$post->title}}</h3>
+                                </div>
+                                <div class="my-2">
+                                        <img src="{{asset("uploads/posts/".$post->featured_img)}}" width="100%" height="150px">
+                                </div><br>
+                                <div>
+                                        {{$post->excerpt}}
+                                </div>
+                                <div class="text-center my-2">
+                                        <a href="{{route('posts.show',[$post->id])}}" class="btn btn-light text-center border border-dark">Read More</a>
+                                </div>
+                                </div>
+                        </div>
+                        @endif
+                        @endforeach
+                </div>
+        </div>
+</div>
+
+
 
 
 @endsection
