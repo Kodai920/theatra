1a6c2dd57a214029e9a64ca9ad7ae7abe7f2c8f3
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-13 16:41:32 +0530

    20200108

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -29,7 +29,13 @@
                 </div>
 
                 <div class="my-5 avg-ovral-rtng">
-                    <h3>みんなの評価 : <i class="fas fa-star"></i> {{ round($movie->reviews->avg('star'),2)}}</h3>
+                    <h3>みんなの評価 : <i class="fas fa-star"></i>
+                        @if($movie->reviews->count() > 0)
+                        {{ round($movie->reviews->avg('star'),2)}}
+                        @else
+                        　ー
+                        @endif
+                    </h3>
                 </div>
 
                 @if(Auth::check())
