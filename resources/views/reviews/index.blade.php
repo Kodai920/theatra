1025fbbd4ffb43e313c3f71454edd6572bcd1210
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-15 12:36:10 +0530

    20200108

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -2,7 +2,7 @@
 @section('image')
 
 <div class="maypage-image mb-5">
-<div class="contaier">
+<div class="contaier m-5">
 <div class="card">
     <div class="card-header">マイページ</div>
     <div class="card-body">
