0f9332319049951ac215945d9cf5c2c17e65dce2
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-15 12:05:57 +0530

    20200108

--- resources/views/countries/index.blade.php
+++ resources/views/countries/index.blade.php
@@ -1,44 +1,11 @@
-
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('', 'Theatra') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ secure_asset('css/custom.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-
-</head>
-<body>
-    <div id="app">
-    @include('inc.navbar')
-    @include('inc.search')
+@extends('layouts.image')
+@section('image')
 
 <div class="country-image">
     <h1 class="ranking-text">製作国</h1>
 </div>
 
 <div class="container">
-
         <div class="card">
                 <div class="card-header">製作国一覧</div>
                 <div class="card-body">
@@ -80,10 +47,6 @@
                     </table>
                 </div>
             </div>
-
 </div>
 
-    @include('inc.footer')
-    </div>
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/image.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('', 'Theatra') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ secure_asset('js/app.js')}}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/custom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    {{-- Insert below other link, style and script tags --}}
+
+   @yield('styles')
+</head>
+<body>
+    <div id="app">
+    @include('inc.navbar')
+    @include('inc.search')
+
+    @yield('image')
+
+    @include('inc.footer')
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+@yield('scripts')
+     <script>
+        @if(Session::has('success'))
+        toastr.success(" {{Session::get('success')}} ")
+        @endif
+        @if(Session::has('info'))
+        toastr.info(" {{Session::get('info')}} ")
+        @endif
+    </script>
+</body>
+</html>
