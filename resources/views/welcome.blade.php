04f977b848fff0a65352cffbc18f8c0ca0d25266
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-16 18:13:06 +0530

    20200108

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('', 'Theatra') }}</title>
+    <title>{{ config('最高のNETFLIXライフを。| Theatra') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
