d417dcd8bd0e15b0ce84f3bce742d9168c09122e
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-16 16:19:30 +0530

    20200108

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,9 +40,9 @@
         <div class="container mb-5">
             <h2 class="welcome-title">THEATRAとは</h2>
             <div class="row">
-                {{-- <div class="col-md-6"> --}}
-                    <img class="welcome-photos col-md-6" src="uploads/welcomes/netflixwel.jpg" alt="">
-                {{-- </div> --}}
+                <div class="col-md-6">
+                    <img class="welcome-photos" src="uploads/welcomes/netflixwel.jpg" alt="">
+                </div>
                 <div class="col-md-6">
                     <p class="function">NETFLIXに特化した、
                         レビュー投稿サイトです。<br>
