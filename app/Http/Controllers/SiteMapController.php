057145213efcba622680c35fdd042275b31c7a8c
kodai920 <kodaiokaoka0920@gmail.com>
2020-01-19 22:06:57 +0530

    20200116

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -9,13 +9,18 @@ use Carbon\Carbon;
 class SiteMapController extends Controller
 {
     // sitemap.xmlの生成と表示
-    public function xml()
-    {
-        $sitemap = \App::make("sitemap");
-        $now = Carbon::now();
-        $sitemap->add(url('/'), $now, '1.0', 'daily');
-        $sitemap->store('xml', 'sitemap');
+    // public function xml()
+    // {
+    //     $sitemap = \App::make("sitemap");
+    //     $now = Carbon::now();
+    //     $sitemap->add(url('/'), $now, '1.0', 'daily');
+    //     $sitemap->store('xml', 'sitemap');
 
-        return $sitemap->render('xml');
-    }
+    //     return $sitemap->render('xml');
+    // }
+
+    public function sitemap()
+{
+  return response()->view('sitemap')->header('Content-Type', 'text/xml');
+}
 }
